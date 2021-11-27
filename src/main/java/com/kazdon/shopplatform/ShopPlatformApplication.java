0969f9d6ac828439e79111e50e41a8e1ddb4659d
Karolina <karolina.zdon@gmail.com>
2021-11-27 12:48:56 +0100

    init

---
+++ src/main/java/com/kazdon/shopplatform/ShopPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.kazdon.shopplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kazdon/shopplatform/ShopPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazdon.shopplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
